d78c330b00eae40a7742d5cdb09950d49f50cc09
Grégory Le Garec <gregory.legarec@gmail.com>
2017-04-11 16:54:32 +0200

    chore: add way to work with local version of cozy-client-js :rocket:

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: '#source-map',
+  externals: ['cozy'],
+  plugins: [
+    new webpack.DefinePlugin({
+      __SERVER__: JSON.stringify('http://app.cozy.local'),
+      __STACK_ASSETS__: false
+    }),
+    new webpack.ProvidePlugin({
+      'cozy.client': 'cozy-client-js/dist/cozy-client.js'
+    })
+  ]
+}
